90d33de6857e60ddbbfc661465c83d686faeaf60
fesahaqillah <haqillahfesa@gmail.com>
2021-11-15 16:17:59 +0700

    database pegawai
    PWEB Week 12 membuat database pegawai menggunakan laravel dengan crud

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,12 @@ Route::get('ets2021',"ViewController@showETS") ;
 Route::get('tugasphp',"ViewController@showPHP") ;
 
 Route::post('ticket',"ViewController@showticket") ;
+
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
